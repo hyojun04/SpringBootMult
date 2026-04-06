c13d5b11cae03b7b56961652bb12f1f348d24893
김민석 <mhn07001@naver.com>
2026-04-06 17:23:47 +0900

    JWT

--- src/main/java/com/example/myapp/config/SecurityConfig.java
+++ src/main/java/com/example/myapp/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.example.myapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.myapp.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	@Autowired
+	JwtAuthenticationFilter authenticationFilter;
+	
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf((csrfConfig) -> csrfConfig.disable());
@@ -27,6 +35,13 @@ public class SecurityConfig {
 				.requestMatchers("/member/insert").permitAll()
 				.requestMatchers("/member/login").permitAll()
 				.requestMatchers("/**").permitAll());
+		
+		// Session 기반의 인증을 사용하지 않고 JWT를 이용하여서 인증
+		http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		// Spring Security JWT 필터 로드
+		http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
 		return http.build();
 	}
 	
